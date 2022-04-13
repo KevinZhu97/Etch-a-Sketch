1ad9befa0b8b618f653354a67b9c6d91d4066da3
Kevin Zhu <kevinzhu@Kevins-Air.cpe.cableonda.net>
2022-04-13 13:51:04 -0500

    Add new feature that can reset game and custom changed the new grid size and add a flavicon image

--- code.js
+++ code.js
@@ -1,28 +1,35 @@
 const container = document.getElementById("container");
 const resetButton = document.querySelector(".reset");
+const newGrid = document.querySelector(".newGrid")
 
-
-// find all divs with class and then proceed to remove them
 resetButton.addEventListener("click", function() {
     Array.from(document.querySelectorAll(".hovered")).forEach((el) => el.classList.remove('hovered'));
 });
 
-function createGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c=0; c < (rows * cols); c++) {
+//delete current grid and replace with new grid + prompt
+newGrid.addEventListener("click", function() {
+    let newSquare = prompt("Please enter new dimensions for new box", "16");
+    while (newSquare > 100) {
+        alert("Please enter a number between 1 and 100.")
+        newSquare = prompt("Please enter new dimensions for new box", "16");
+    }
+    container.innerHTML = "";
+    createGrid(newSquare);
+});
+
+function createGrid(side) {
+    container.style.setProperty('--grid-rows', side);
+    container.style.setProperty('--grid-cols', side);
+    for (c=0; c < (side * side); c++) {
         const cell = document.createElement('div');
         cell.addEventListener('mouseenter', function(event) {
             this.classList.add('hovered');
         });
-        cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item";
     };
-
 };
 
-function purple() {
-    item.setAttribute('style', 'background-color:blue;')
-}
+   
 
-createGrid(16, 16);
+
+createGrid(16);
